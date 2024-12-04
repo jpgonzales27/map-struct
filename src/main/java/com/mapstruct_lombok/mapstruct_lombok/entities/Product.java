30be255cef23c08a465e9d8569635949035bc0e1
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-12-04 09:06:00 -0400

    configurando mapstruct y lombok para que trabajen juntos

--- src/main/java/com/mapstruct_lombok/mapstruct_lombok/entities/Product.java
+++ src/main/java/com/mapstruct_lombok/mapstruct_lombok/entities/Product.java
@@ -1,12 +1,16 @@
 package com.mapstruct_lombok.mapstruct_lombok.entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
 
     @Id
@@ -16,36 +20,4 @@ public class Product {
 
     private LocalDateTime creationDate;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", creationDate=" + creationDate +
-                '}';
-    }
 }
